e64275f6cabf05c23940abd2f935cc89a33e484c
Jolsen6 <olsenjeremiah06@gmail.com>
2023-02-13 02:12:58 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,11 @@
+#include "main.h"
+
 /**
  * _memcpy - function that copies memory area
  * @dest: destination
  * @src: source
  * @n: number of bytes
- * Return: if successful
+ * Return: pointer to destination
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n);
