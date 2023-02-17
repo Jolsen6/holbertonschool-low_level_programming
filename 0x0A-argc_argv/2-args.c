b9b1ae1425aa2cf43eba3fc56fb6b41978c1f487
Jolsen6 <olsenjeremiah06@gmail.com>
2023-02-17 15:57:36 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,15 @@
   * @argv: argument vector
   * Return: Always zero
   */
+
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
