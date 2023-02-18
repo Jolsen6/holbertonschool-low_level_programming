089f8bc482672cd100290d86b815d869eb02fc9d
Jolsen6 <olsenjeremiah06@gmail.com>
2023-02-17 16:27:49 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,19 @@
 
 int main (int argc, char *argv[])
 {
-int i = 0;
-int d = 0;
-int sum = 0;
-for (i = 1; i < argc; i++)
-{
-for (d = 0;argv[i][d]; d++)
-{
-if (argv [i][d] < '0' || argv[i][d] > '9')
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[i]);
-}
-printf("%d\n", sum);
-return (0);
+int i, d, sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+	for (d = 0;argv[i][d]; d++)
+		{
+		if (argv [i][d] < '0' || argv[i][d] > '9')
+			{
+			printf("Error\n");
+			return (1);
+			}
+		}
+	sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
