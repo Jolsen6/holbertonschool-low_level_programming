7f6117c6665ada9bfc86cad666f13c953b5e68a0
Jolsen6 <olsenjeremiah06@gmail.com>
2023-02-27 10:13:50 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,9 @@
 */
 void *malloc_checked(unsigned int b)
 {
-void *p = malloc(b);
-
-if (p == NULL)
-{
-free(p);
-exit(98);
-}
-
-return (p);
+	void *p;
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
