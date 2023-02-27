63f1833453f9fef0ced8f4592d2cc219d9b85625
Jolsen6 <olsenjeremiah06@gmail.com>
2023-02-27 09:54:18 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,15 +5,17 @@
 /**
 *malloc_checked - allocates memory
 *@b: size of memory
-*return: pointer to new mem
+*return: nothing but my own despair
 */
 void *malloc_checked(unsigned int b)
 {
 void *p = malloc(b);
+
 if (p == NULL)
 {
 free(p);
 exit(98);
 }
+
 return (p);
 }
