dca0f6ff539c7fba2c98d73c6915fa620f999c19
Jolsen6 <olsenjeremiah06@gmail.com>
2023-02-27 09:48:59 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
 *malloc_checked - allocates memory
 *@b: size of memory
-*return: pointer to new memory
+*return: pointer to new mem
 */
-
 void *malloc_checked(unsigned int b)
 {
-void *p;
-p = malloc(b);
+void *p = malloc(b);
 if (p == NULL)
+{
+free(p);
 exit(98);
 }
 return (p);
